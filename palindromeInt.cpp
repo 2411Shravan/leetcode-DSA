4749bb1d7d690b65fc7b726983f3ae0753f13380
Shravan .V <perfectparameter24@gmail.com>
2021-08-27 20:47:50 +0530

    palindrome check using cpp

---
+++ palindromeInt.cpp
@@ -0,0 +1,59 @@
+// Given an integer x, return true if x is palindrome integer.
+
+// An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+// Example 4:
+
+// Input: x = -101
+// Output: false
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+
+//SOLUTION : -
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x == 0) {
+            return true;
+        }
+        if(x < 0 || x%10 == 0)
+            return false;
+        
+        int temp = 0;
+        int preX = x;
+        while (x > temp) {
+            int pop = x%10;
+            preX = x;
+            x /= 10;
+
+            temp = temp*10 + pop;
+        }
+        if(x == temp || preX == temp)
+            return true;
+        else
+            return false;
+        
+        
+            
+    }
+};

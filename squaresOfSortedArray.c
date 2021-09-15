9de44c1e9b6fd5216f2a2f17abf971132e191041
Shravan .V <perfectparameter24@gmail.com>
2021-09-15 21:00:37 +0530

    return square of given array using c language

---
+++ squaresOfSortedArray.c
@@ -0,0 +1,51 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+// Example 2:
+
+// Input: nums = [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums is sorted in non-decreasing order.
+
+
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sortedSquares(int* nums, int numsSize, int* returnSize){
+    *returnSize=numsSize;
+    int *arr;
+    arr=(int*)malloc(*returnSize*sizeof(int*));
+    for(int i=0;i<numsSize;i++){
+        arr[i]=pow(nums[i],2);
+    }
+       int counter=1;
+    while(counter<numsSize){
+        for(int i=0;i<numsSize-counter;i++){
+            if(arr[i]>arr[i+1]){
+                int temp;
+                temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+            
+    }
+    counter++;
+}
+    
+    return arr;
+}

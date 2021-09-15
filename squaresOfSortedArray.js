403996f836efcac93a11fa193ea6da55ea641128
Shravan .V <perfectparameter24@gmail.com>
2021-09-15 19:46:53 +0530

    return square of given array using javascript

---
+++ squaresOfSortedArray.js
@@ -0,0 +1,38 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+// Example 2:
+
+// Input: nums = [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums is sorted in non-decreasing order.
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortedSquares = function(nums) {
+    var req=nums.length;
+    var final_data=[req];
+    
+    for(var i=0;i<req;i++){
+        final_data[i]=nums[i]*nums[i];
+    }
+    final_data.sort(function(a, b){return a - b});
+    console.log(final_data);
+    return final_data;
+    
+};

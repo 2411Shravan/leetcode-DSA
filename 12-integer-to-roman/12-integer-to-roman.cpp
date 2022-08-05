507527fc82050f9d4f37e3eec2dac1b0387a0b67
Shravan .V <68394410+2411Shravan@users.noreply.github.com>
2022-08-05 22:34:49 +0530

    Time: 26 ms (17.98%), Space: 12.9 MB (7.79%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,13 +1,27 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        int numbers[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        string roman[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        //int numbers[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        //string roman[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        vector<pair<int,string>> mp;
+        mp.push_back({1000,"M"});
+        mp.push_back({900,"CM"});
+        mp.push_back({500,"D"});
+        mp.push_back({400,"CD"});
+        mp.push_back({100,"C"});
+        mp.push_back({90,"XC"});
+        mp.push_back({50,"L"});
+        mp.push_back({40,"XL"});
+        mp.push_back({10,"X"});
+        mp.push_back({9,"IX"});
+        mp.push_back({5,"V"});
+        mp.push_back({4,"IV"});
+        mp.push_back({1,"I"});
         string ret_ans="";
         for(int i=0;num;i++){
-            while(num>=numbers[i]){
-                ret_ans+=roman[i];
-                num-=numbers[i];
+            while(num>=mp[i].first){
+                ret_ans+=mp[i].second;
+                num-=mp[i].first;
             }
         }
         

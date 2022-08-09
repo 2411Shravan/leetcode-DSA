d51ebb3a3acc7509b7868e937f5233fb56f1df12
Shravan .V <68394410+2411Shravan@users.noreply.github.com>
2022-08-09 10:46:36 +0530

    Time: 6 ms (90.60%), Space: 7.3 MB (45.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -6,62 +6,59 @@ public:
             return false;
         }
         
-        unordered_map<char,int> firstS;
-        unordered_map<char,int> secondT;
+//         unordered_map<char,int> firstS;
+//         unordered_map<char,int> secondT;
+        
+//         for(int i=0;i<s.size();i++){
+//             if(firstS.find(s[i])!=firstS.end()){
+//                 firstS[s[i]]=firstS[s[i]]+1;
+//             }
+//             else{
+//                 firstS[s[i]]=1;
+//             }
+//         }
+        
+//         for(int j=0;j<t.size();j++){
+//             if(secondT.find(t[j])!=secondT.end()){
+//                 secondT[t[j]]=secondT[t[j]]+1;
+//             }
+//             else{
+//                 secondT[t[j]]=1;
+//             }
+//         }
+        
+//         int i=0;
+//         for(auto it:firstS){
+//             cout<<it.first<<"->"<<it.second<<endl;
+//         }
+//           cout<<"------"<<endl;
+        
+//         for(auto its:secondT){
+//             cout<<its.first<<"->"<<its.second<<endl;
+//         }
+        
+//         unordered_map<char,int>::iterator ti;
+        
+//         for(ti=firstS.begin();ti!=firstS.end();++ti){
+//            if(ti->second==secondT[ti->first]){
+//                continue;
+//            }
+//             else{
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+        
+        //-----> without using hashmap,
+        vector<int> firstS(26,0);
+        vector<int> secondT(26,0);
         
         for(int i=0;i<s.size();i++){
-            if(firstS.find(s[i])!=firstS.end()){
-                firstS[s[i]]=firstS[s[i]]+1;
-            }
-            else{
-                firstS[s[i]]=1;
-            }
+            firstS[s[i]-'a']++;
+            secondT[t[i]-'a']++;
         }
         
-        for(int j=0;j<t.size();j++){
-            if(secondT.find(t[j])!=secondT.end()){
-                secondT[t[j]]=secondT[t[j]]+1;
-            }
-            else{
-                secondT[t[j]]=1;
-            }
-        }
-        
-        int i=0;
-        for(auto it:firstS){
-            cout<<it.first<<"->"<<it.second<<endl;
-        }
-          cout<<"------"<<endl;
-        
-        for(auto its:secondT){
-            cout<<its.first<<"->"<<its.second<<endl;
-        }
-        
-        int flag=1;
-        // for(auto it: firstS){
-        //     if((secondT.find(it.first)!=secondT.end()) && (it.second != secondT[it.first])){
-        //         continue;
-        //     }
-        //     else{
-        //         flag=0;
-        //         return false;
-        //     }
-        //     // i++;
-        // }
-        // if(flag){
-        //     cout<<"found all"<<endl;
-        // }
-        unordered_map<char,int>::iterator ti;
-        
-        for(ti=firstS.begin();ti!=firstS.end();++ti){
-           if(ti->second==secondT[ti->first]){
-               continue;
-           }
-            else{
-                return false;
-            }
-        }
-        
-        return true;
+        return firstS==secondT;
     }
 };
